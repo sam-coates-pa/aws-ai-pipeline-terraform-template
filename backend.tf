ee3451771f576aa774499edc03e28086131a641a
sam-coates-pa <138590259+sam-coates-pa@users.noreply.github.com>
2026-02-23 17:18:03 +0000

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-fraud"
+    key            = "dev/state/terraform.tfstate"
+    region         = "eu-west-2"
+    use_lockfile = true
+  }
+}
